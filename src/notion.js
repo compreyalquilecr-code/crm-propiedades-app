799cf63f8a91666ae5d8fefa3731c1107ddc3ac8
Compre y Alquile <compreyalquilecr@gmail.com>
2026-06-05 10:33:46 -0600

    Corrigiendo URL API

--- src/notion.js
+++ src/notion.js
@@ -1,9 +1,11 @@
 export async function getPropiedades() {
+  const response = await fetch(
+    "https://crm-propiedades-app.onrender.com/propiedades"
+  );
 
-const response = await fetch(
-"https://crm-propiedades-app.onrender.com/propiedades"
-);
+  if (!response.ok) {
+    throw new Error("Error al cargar propiedades");
+  }
 
-return await response.json();
-
+  return await response.json();
 }
